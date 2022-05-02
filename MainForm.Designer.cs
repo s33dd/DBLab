cf16a89fc0c7be6050e971a7f53d7431f8e9914f
s33dd <dsuhoverikov@gmail.com>
2022-05-02 20:17:24 +0200

    reading objects

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -25,10 +25,10 @@
     private void InitializeComponent() {
       this.MenuStrip = new System.Windows.Forms.MenuStrip();
       this.BaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.AddObjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.SaveListOfObjectsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-      this.ObjectList = new System.Windows.Forms.GroupBox();
+      this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.ObjectList = new System.Windows.Forms.TableLayoutPanel();
       this.MenuStrip.SuspendLayout();
       this.SuspendLayout();
       // 
@@ -40,7 +40,7 @@
             this.AboutToolStripMenuItem});
       this.MenuStrip.Location = new System.Drawing.Point(0, 0);
       this.MenuStrip.Name = "MenuStrip";
-      this.MenuStrip.Size = new System.Drawing.Size(800, 24);
+      this.MenuStrip.Size = new System.Drawing.Size(783, 24);
       this.MenuStrip.TabIndex = 0;
       // 
       // BaseToolStripMenuItem
@@ -51,13 +51,6 @@
       this.BaseToolStripMenuItem.Name = "BaseToolStripMenuItem";
       this.BaseToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
       this.BaseToolStripMenuItem.Text = "Base";
-      // 
-      // AboutToolStripMenuItem
-      // 
-      this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
-      this.AboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
-      this.AboutToolStripMenuItem.Text = "About";
-      this.AboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
       // 
       // AddObjectToolStripMenuItem
       // 
@@ -71,18 +64,34 @@
       this.SaveListOfObjectsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
       this.SaveListOfObjectsToolStripMenuItem.Text = "Save list of objects";
       // 
+      // AboutToolStripMenuItem
+      // 
+      this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+      this.AboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+      this.AboutToolStripMenuItem.Text = "About";
+      this.AboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+      // 
       // ObjectList
       // 
-      this.ObjectList.Location = new System.Drawing.Point(13, 28);
+      this.ObjectList.AutoScroll = true;
+      this.ObjectList.AutoSize = true;
+      this.ObjectList.ColumnCount = 3;
+      this.ObjectList.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+      this.ObjectList.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+      this.ObjectList.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+      this.ObjectList.Location = new System.Drawing.Point(12, 27);
       this.ObjectList.Name = "ObjectList";
-      this.ObjectList.Size = new System.Drawing.Size(775, 410);
+      this.ObjectList.RowCount = 1;
+      this.ObjectList.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+      this.ObjectList.Size = new System.Drawing.Size(764, 41);
       this.ObjectList.TabIndex = 1;
-      this.ObjectList.TabStop = false;
       // 
       // MainForm
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.AutoScroll = true;
+      this.AutoScrollMinSize = new System.Drawing.Size(0, 489);
       this.BackColor = System.Drawing.SystemColors.HighlightText;
       this.ClientSize = new System.Drawing.Size(800, 450);
       this.Controls.Add(this.ObjectList);
@@ -105,7 +114,7 @@
     private System.Windows.Forms.ToolStripMenuItem AddObjectToolStripMenuItem;
     private System.Windows.Forms.ToolStripMenuItem SaveListOfObjectsToolStripMenuItem;
     private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
-    private System.Windows.Forms.GroupBox ObjectList;
+    private System.Windows.Forms.TableLayoutPanel ObjectList;
   }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Data.Sqlite;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace DBLab {
   public partial class MainForm : Form {
     private string dbRoute = @"Data source=.\spaceobjects.db;Mode=ReadWrite";
+    private List<SpaceObject> objects;
 
     public MainForm() {
       InitializeComponent();
@@ -17,6 +19,7 @@ namespace DBLab {
     }
 
     private void MainForm_Load(object sender, EventArgs e) {
+      objects = new List<SpaceObject>();
       if (!Properties.Settings.Default.NotShow) {
         AboutForm about = new AboutForm();
         about.ShowDialog();
@@ -28,8 +31,36 @@ namespace DBLab {
         catch {
           MessageBox.Show("Database not found. Please, check all files.", "Error!");
           Close();
+          return;
+        }
+        string query = "SELECT * FROM Object";
+        SqliteCommand command = new SqliteCommand(query, db);
+        using (SqliteDataReader reader = command.ExecuteReader()) {
+          if (reader.HasRows) {
+            while (reader.Read()) {
+              SpaceObject obj = new SpaceObject((int)reader["id"]);
+              obj.Name = reader["name"].ToString();
+              obj.Description = reader["description"].ToString();
+              obj.Type = reader["type"].ToString();
+              obj.Location = reader["location"].ToString();
+              obj.Photo = reader["photo"].ToString();
+              objects.Add(obj);
+            }
+          }
+          else {
+            MessageBox.Show("Database is empty. Add something.", "Warning!");
+          }
         }
       }
+/*      for (int i = 1; i <= 90; i++) {
+        Label name = new Label();
+        name.Text = $"asdasd{i}";
+        ObjectList.Controls.Add(name);
+        if (i % 3 == 0) {
+          ObjectList.RowCount = ObjectList.RowCount + 1;
+          ObjectList.RowStyles.Add(new RowStyle(SizeType.Percent, 100));
+        }
+      }*/
     }
   }
 }
